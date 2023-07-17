bdea7a7877f8450b89da09de8df5bb50dd173ed7
nnasa14 <130805007+nnasa14@users.noreply.github.com>
2023-07-16 20:25:04 -0400

    Moved random word generator to init function

--- body.py
+++ body.py
@@ -1,25 +1,15 @@
-#generate word
-#print the gallows and empty letter spaces
-#ask for user input
-#respond to incorrect by adding the hangman onto gallows
-#respond to correct input by adding letter into the correct position(s)
-#repeat until either the hangman is completed or the word is solved
-
-#randomly select a word from the json file
-#print the gallows and amount of chars in the word (spaces will stay as spaces)
-#ask for user input
-#if input is in word, replace _ with char when appropriate
-#else, print a limb on the gallows (add a counter to keep track of this)
-#when counter reaches its end point, print you lose and reveal the word
-#when len of puzzle == len word, print you won
 import random
 
 class Hangman:
     def __init__(self):
-        self.puzzle = []
+        with open("words.json", 'r') as file:
+            word_list = "words.json"(file)
+
+        word = random.choice(word_list)
+        self.word = word
 
     def __str__(self):
-        return f"{self.puzzle}"
+        return f"{self.word}"
 
     def random_word(self):
         with open("words.json", 'r') as file:
@@ -29,15 +19,15 @@ class Hangman:
         return random_word
 
     def test_char(self, char):
-        for item in word.lower():
+        for item in self.word.lower():
             if item == char:
                 Hangman().printPuzzle(char)     #print it on the approppriate position
             else:
-                attempts =+ 1
-                return f"Incorrect. You have {attempts} attempts left."
+                incorrect_attempts =+ 1
+                return f"Incorrect. You have {incorrect_attempts} attempts left."
     
-    def printPuzzle(self, char):
-        for item in word:
+    def printPuzzle(self, char):                #need to fix this to rememeber previous inputs
+        for item in self.word:
             if char == " ":
                 print(item)
             if char == item:
@@ -47,9 +37,8 @@ class Hangman:
         
 
 if __name__ == "__main__":
-    hangman = Hangman()
-    
-    word = hangman.random_word        #get random string as a global variable
+    hangman_instance = Hangman()
+    target_word = hangman_instance.word
 
     difficulty = input("Please select your difficulty: (hard, normal, easy)")
     if difficulty == "hard":
@@ -61,15 +50,17 @@ if __name__ == "__main__":
     else:
         print('Please run again and type "hard", "normal", or "easy".')
 
+    incorrect_attempts = 0
+    puzzle = ""
 
     while True:
         if guesses == 0:
             break
-        puzzle = 0
-        attempts = 0
+
         char = input("Guess a letter: ")
-        print(Hangman.test_char)       #test char
-        if len(puzzle) == len(word):
+        print(Hangman.test_char)                            #test char
+
+        if len(puzzle) == len(target_word):
             print("You won in {guesses} guesses!")
-        if attempts == guesses:
+        if incorrect_attempts == guesses:
             print("You lost. The correct answer is: {word}")
