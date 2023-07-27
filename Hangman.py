0272677b4af8e7ee4449cf836ed11823654dc682
nnasa14 <nnascimento.kr@gmail.com>
2023-07-27 15:48:20 -0400

    more GUI changes to hangman.py

--- Hangman.py
+++ Hangman.py
@@ -27,6 +27,9 @@ class Hangman:
         else:
             self.word = None
             self.puzzle = []
+        self.guesses = []
+        self.max_attempts = 6
+        self.attempts_left = self.max_attempts
 
         self.canvas = tk.Canvas(width=300, height=300)
         self.canvas.pack()
@@ -38,7 +41,9 @@ class Hangman:
         return f"{self.word}"
     
     def new_game(self):
-        self.word = Hangman.get_word(self)
+        self.word = random.choice(self.word)
+        self.guesses = []
+        self.attempts_left = self.max_attempts
         self.draw_gallows()
 
         self.word_display = tk.StringVar()
@@ -58,42 +63,30 @@ class Hangman:
         x0, y0 = 20, 280
         x1, y1 = 120, 280
 
-        self.canvas.create_line(x0, y0, x1, y1, tags="hangman")
-        self.canvas.create_line(x0 + 50, y0, x1 + 50, y1, tags="hangman")
-        self.canvas.create_line(x0, y0, x0 + 50, y0, tags="hangman")
-        self.canvas.create_line(x0, y0 - 200, x0, y0, tags="hangman")
-        self.canvas.create_line(x1, y0 - 200, x1, y0, tags="hangman")
+        if self.attempts_left < self.max_attempts:
+            self.canvas.create_line(x0, y0, x1, y1, tags="hangman")
+            self.canvas.create_line(x0 + 50, y0, x1 + 50, y1, tags="hangman")
+            self.canvas.create_line(x0, y0, x0 + 50, y0, tags="hangman")
+            self.canvas.create_line(x0, y0 - 200, x0, y0, tags="hangman")
+            self.canvas.create_line(x1, y0 - 200, x1, y0, tags="hangman")
 
-    def draw_head(self):
-        x0, y0 = 20, 280
-        x1 = 120
+        if self.attempts_left < self.max_attempts - 1:
+            self.canvas.create_oval(x0 + 25, y0 - 200, x1 - 25, y0 - 150, tags="hangman")
 
-        self.canvas.create_oval(x0 + 25, y0 - 200, x1 - 25, y0 - 150, tags="hangman")
+        if self.attempts_left < self.max_attempts - 2:
+            self.canvas.create_line(x0 + 50, y0 - 150, x0 + 50, y0 - 100, tags="hangman")
 
-    def draw_right_arm(self):
-        x0, y0 = 20, 280
+        if self.attempts_left < self.max_attempts - 3:
+            self.canvas.create_line(x0 + 50, y0 - 100, x0 + 25, y0 - 75, tags="hangman")
 
-        self.canvas.create_line(x0 + 50, y0 - 150, x0 + 50, y0 - 100, tags="hangman")
+        if self.attempts_left < self.max_attempts - 4:
+            self.canvas.create_line(x0 + 50, y0 - 100, x0 + 75, y0 - 75, tags="hangman")
 
-    def draw_left_arm(self):
-        x0, y0 = 20, 280
+        if self.attempts_left < self.max_attempts - 5:
+            self.canvas.create_line(x0 + 50, y0 - 200, x0 + 25, y0 - 225, tags="hangman")
 
-        self.canvas.create_line(x0 + 50, y0 - 100, x0 + 25, y0 - 75, tags="hangman")
-
-    def draw_left_leg(self):
-        x0, y0 = 20, 280
-
-        self.canvas.create_line(x0 + 50, y0 - 100, x0 + 75, y0 - 75, tags="hangman")
-
-    def draw_right_leg(self):
-        x0, y0 = 20, 280
-
-        self.canvas.create_line(x0 + 50, y0 - 200, x0 + 25, y0 - 225, tags="hangman")
-    
-    def draw_ending(self):
-        x0, y0 = 20, 280
-
-        self.canvas.create_line(x0 + 50, y0 - 200, x0 + 75, y0 - 225, tags="hangman")
+        if self.attempts_left < self.max_attempts - 6:
+            self.canvas.create_line(x0 + 50, y0 - 200, x0 + 75, y0 - 225, tags="hangman")
 
     def restart(self):
         self.word_label.pack_forget()
@@ -130,21 +123,31 @@ class Hangman:
         """
         for item in self.word.lower():
             if item == char:
-                self.editPuzzle(char)     
-                return self.puzzle
+                return self.update_word_display() 
     
         return False
     
-    def editPuzzle(self, char):
-        """
-        Function that adds a character to its corresponding position in self.word in self.puzzle
+    def update_word_display(self):
+        displayed_word = ""
+        for letter in self.word:
+            if letter in self.guesses:
+                displayed_word += letter + " "
+            else:
+                displayed_word += "_ "
+        self.word_display.set(displayed_word.strip())
 
-        Parameters:
-            char(str): Character that was validated in test_char
-        """
-        for index, item in enumerate(self.word):
-            if char == item:
-                self.puzzle[index] = char
+        if all(letter in self.guesses for letter in self.word):
+            messagebox.showinfo("Congratulations", "You won!")
+            self.restart()
+
+        elif self.attempts_left == 0:
+            messagebox.showinfo("Game Over", f"You lost. The word was '{self.word}'.")
+            self.restart()
+
+        else:
+            if self.guesses[-1] not in self.word:
+                self.attempts_left -= 1
+                self.draw_gallows()
 
     def guessWord(self, guess):
         """
@@ -161,5 +164,17 @@ class Hangman:
             return True
         else:
             self.incorrect_attempts =+ 1
-            print(f"Your guess, {guess}, was incorrect.")
+            messagebox.showinfo(f"Your guess, {guess}, was incorrect.")
             return
+        
+    def check_guess(self):          #checks if guess if a valid input and appends to guesses list
+        guess = self.guess_entry.get()
+        if guess and guess.isalpha() and len(guess) == 1:
+            if guess not in self.guesses:
+                self.guesses.append(guess)
+                self.update_word_display()
+            else:
+                messagebox.showinfo("Invalid Guess", "You've already guessed that letter.")
+        else:
+            messagebox.showinfo("Invalid Guess", "Please enter a single alphabetical character.")
+        self.guess_entry.delete(0, tk.END)
--- body.py
+++ body.py
@@ -17,34 +17,16 @@ if __name__ == "__main__":
 
     puzzle_list = hangman_instance.puzzle
 
-    print(puzzle_list)
+    messagebox.showinfo(puzzle_list)
 
     while True:
         char_guess = str(input("Guess a letter: "))
         char_guess_return = hangman_instance.test_char(char_guess)     
-        print(puzzle_list)   
+        messagebox.showinfo(puzzle_list)   
         attempts += 1             
 
-        hangman_instance.draw_gallows
-
-        if char_guess_return == False:
-            incorrect_attempts += 1
-            print(f"Incorrect. You have {(guesses - incorrect_attempts)} attempts left.")
-            if incorrect_attempts == 1:
-                hangman_instance.draw_head
-            elif incorrect_attempts == 2:
-                hangman_instance.draw_left_arm   
-            elif incorrect_attempts == 3:
-                hangman_instance.draw_right_arm   
-            elif incorrect_attempts == 4:
-                hangman_instance.draw_left_leg   
-            elif incorrect_attempts == 5:
-                hangman_instance.draw_right_leg   
-            elif incorrect_attempts == 6:
-                hangman_instance.draw_ending   
-
         if incorrect_attempts == guesses:
-            print(f"You lost. The correct answer is: {target_word}")
+            messagebox.showinfo(f"You lost. The correct answer is: {target_word}")
             break    
 
         puzzle_str = ""
@@ -58,5 +40,5 @@ if __name__ == "__main__":
             guess_word_return = hangman_instance.guessWord(input_guess)
 
         if puzzle_str == target_word or guess_word_return == True:  #determine if completed inputs or guessed word is correct
-            print(f"You won in {attempts} guesses!")
+            messagebox.showinfo(f"You won in {attempts} guesses!")
             break
