2ef166d1169cf61a5fb251c3cc052ac2224802ac
nnasa14 <nnascimento.kr@gmail.com>
2023-08-03 11:53:58 -0400

    Gallows.py minor edits

--- Gallows.py
+++ Gallows.py
@@ -38,31 +38,3 @@ class Gallows:
 
     def right_leg(self):
         self.canvas.create_line(self.x1, self.y0 - 75, self.x1 + 25, self.y0, tags="hangman")
-
-    def draw_gallows(self):
-        self.canvas.delete("hangman")
-        x0, y0 = 50, 280
-        x1, y1 = 150, 280
-
-        # Draw the main structure
-        if self.attempts_left < self.max_attempts:
-            self.canvas.create_line(x0, y0, x1, y1, tags="hangman")      # Bottom line
-            self.canvas.create_line(x0 + 50, y0, x1 + 50, y1, tags="hangman")  # Vertical line
-            self.canvas.create_line(x0, y0 - 300, x0, y0, tags="hangman")  # Left vertical line
-            self.canvas.create_line(x1, y0 - 250, x1, y0 - 220, tags="hangman")  # Right vertical line
-
-        # Draw the left arm
-        if self.attempts_left < self.max_attempts - 3:
-            self.canvas.create_line(x1, y0 - 170, x1 - 50, y0 - 150, tags="hangman")
-
-        # Draw the right arm
-        if self.attempts_left < self.max_attempts - 4:
-            self.canvas.create_line(x1, y0 - 170, x1 + 50, y0 - 150, tags="hangman")
-
-        # Draw the left leg
-        if self.attempts_left < self.max_attempts - 5:
-            self.canvas.create_line(x1, y0 - 75, x1 - 25, y0, tags="hangman")
-
-        # Draw the right leg
-        if self.attempts_left < self.max_attempts - 6:
-            self.canvas.create_line(x1, y0 - 75, x1 + 25, y0, tags="hangman")
